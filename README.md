b42e3f404e428bf819006a92c5c2a8389a0a769f
Babak Naderi <babak.naderi@tu-berlin.de>
2019-10-22 23:47:35 +0200

    scipt to create audio clips for environment jnd test

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# P808 Implmentation
+# Implementation of ITU-T P.808
 Implementation of the ITU-T P.808 Recommendation to be used in Amazon Mechanical Turk platform.
---
+++ Tests/Environment Test/script/create_jnd_dataset.py
@@ -0,0 +1,33 @@
+"""
+@author: Babak Naderi
+"""
+
+from audiolib import audioread, audiowrite, snr_mixer
+from os.path import isfile, join,  basename
+import os
+import numpy as np
+
+
+"""
+Given a source folder, add white-noise in a range of different SNR levels to all files in the source folder.
+"""
+
+if __name__=="__main__":
+
+    source_folder = "p501/en-US"
+    source_files = [join(source_folder, f) for f in os.listdir(source_folder) if isfile(join(source_folder, f))]
+    output_folder = source_folder+"_snr"
+    snr_min = 30
+    snr_max = 51
+
+    for f in source_files:
+        clean, fs = audioread(f)
+        # white-noise
+        noise = np.random.normal(0, 1, len(clean))
+        for i in range (snr_min,snr_max):
+            clean_snr, noise_snr, noisy_snr = snr_mixer(clean=clean, noise=noise, snr=i)
+            output_filename = join(output_folder,
+                                   f'{i}S_{os.path.splitext(basename(f))[0]}.wav')
+            audiowrite(noisy_snr, fs,output_filename, norm=False)
+
+
