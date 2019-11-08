c60819a23b312ff9cfd409250734a1fbf6480101
Babak Naderi <babak.naderi@tu-berlin.de>
2019-11-08 01:07:31 +0100

    add license header to all source code files

--- Scripts/create_trapping_stimuli.py
+++ Scripts/create_trapping_stimuli.py
@@ -1,6 +1,11 @@
 """
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
 @author: Babak Naderi
 """
+
 import argparse
 from os.path import isfile, join, basename, dirname
 import os
--- Scripts/process_qualification_ans.py
+++ Scripts/process_qualification_ans.py
@@ -1,6 +1,11 @@
 """
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
 @author: Babak Naderi
 """
+
 import argparse
 import csv
 import os
