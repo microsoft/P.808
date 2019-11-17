fd89b476ad85e71f299a4070a676e7eab5d4cd29
Babak Naderi <babak.naderi@tu-berlin.de>
2019-11-17 23:27:19 +0100

    add coments to acr_result_pasrser

--- Scripts/acr_result_parser.py
+++ Scripts/acr_result_parser.py
@@ -14,42 +14,43 @@ import argparse
 import os
 import numpy as np
 import sys
+from scipy import stats
 
-config= {
-    'math': {
-     'math1.wav': 3,
-     'math2.wav': 7,
-     'math3.wav': 6,
+config = {
+    "math": {
+     "math1.wav": 3,
+     "math2.wav": 7,
+     "math3.wav": 6,
     },
-    'question_names': [ 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10', 'q11', 'q12'],
-    'expected_votes_per_file': 10,
-    'trapping': {
-        'url_found_in': 'input.tp',
-        'ans_found_in': 'input.tp_ans',
+    "question_names": ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12"],
+    "expected_votes_per_file": 10,
+    "trapping": {
+        "url_found_in": "input.tp",
+        "ans_found_in": "input.tp_ans",
     },
-    'gold_question': {
-        'url_found_in': 'input.gold_clips',
-        #'ans_found_in': 'input.gold_clips_ans',
-        'correct_ans': 5,
-        'variance': 1
+    "gold_question": {
+        "url_found_in": "input.gold_clips",
+        #"ans_found_in": "input.gold_clips_ans",
+        "correct_ans": 5,
+        "variance": 1
     },
-
-    'acceptance_criteria': {
-        'all_audio_played_equal': 1,
-        'correct_math_bigger_equal': 1,
-        'correct_tps_bigger_equal': 1,
-        'variance_bigger_equal': 0.1,
-        #'correct_gold_q_bigger_equal': 1
-    }, 'accept_and_use': {
+    "acceptance_criteria": {
+        "all_audio_played_equal": 1,
+        "correct_math_bigger_equal": 1,
+        "correct_tps_bigger_equal": 1,
+        "variance_bigger_equal": 0.1,
+        #"correct_gold_q_bigger_equal": 1
+    },
+    "accept_and_use": {
         # including acceptance_criteria
-        'gold_standard_bigger_equal': 1,
-        'correct_cmp_bigger_equal': 2,
+        "gold_standard_bigger_equal": 1,
+        "correct_cmp_bigger_equal": 2,
     },
-    'bonus': {
-        'when_HITs_more_than': 30,
-        'extra_pay_per_HIT': 0.25
+    "bonus": {
+        "when_HITs_more_than": 30,
+        "extra_pay_per_HIT": 0.25
     },
-    'rejection_feedback': "Answer to this assigmnet did not pass the quality control "
+    "rejection_feedback": "Answer to this assigmnet did not pass the quality control "
                           "check. Make sure to use headset (wear both earbuds), perform the task in quiet environment,"
                           " and use the entire scale range.",
     #"book_04753_chp_0059_reader_10797_6.wav" mask "????????????????????????????????xxxxxx":book_04753_chp_0059_reader_10797
@@ -59,7 +60,7 @@ config= {
 
 def outliers_modified_z_score(votes):
     """
-    return  outliers, cleans
+    return  outliers, using modified z-score
     :param votes:
     :return:
     """
@@ -79,8 +80,27 @@ def outliers_modified_z_score(votes):
     return v
 
 
-def check_if_session_accepted(data):
+def outliers_z_score(votes):
+    """
+    return  outliers, using z-score
+    :param votes:
+    :return:
+    """
+    threshold = 3.29
+    z = np.abs(stats.zscore(votes))
 
+    x = np.array(z)
+    v = np.array(votes)
+    v = v[(x < threshold)]
+    return v
+
+
+def check_if_session_accepted(data):
+    """
+    Check if the session can be acceptd given the criteria in config and the calculations
+    :param data:
+    :return:
+    """
     if data['all_audio_played'] == config['acceptance_criteria']['all_audio_played_equal'] and \
         (data['correct_math'] is None or data['correct_math'] >= config['acceptance_criteria']['correct_math_bigger_equal']) and \
         data['correct_tps'] >= config['acceptance_criteria']['correct_tps_bigger_equal'] and \
@@ -90,6 +110,11 @@ def check_if_session_accepted(data):
 
 
 def check_if_session_should_be_used(data):
+    """
+    Check if the session should be used given the criteria in config
+    :param data:
+    :return:
+    """
     if data['accept'] == 1 and \
             data['correct_gold_question']>= config['accept_and_use']['gold_standard_bigger_equal'] and \
             (data['correct_cmps'] is None or \
@@ -99,7 +124,12 @@ def check_if_session_should_be_used(data):
 
 
 def check_audio_played(row):
-    question_played=0
+    """
+    check if all audios for questions played until the end
+    :param row:
+    :return:
+    """
+    question_played = 0
     try:
         for q_name in config['question_names']:
             if int(row[f'answer.audio_n_finish_{q_name}']) > 0:
@@ -110,8 +140,12 @@ def check_audio_played(row):
 
 
 def check_tps(row):
+    """
+    Check if the trapping questions are answered correctly
+    :param row:
+    :return:
+    """
     correct_tps = 0
-
     tp_url = row[config['trapping']['url_found_in']]
     tp_correct_ans = int(float(row[config['trapping']['ans_found_in']]))
     try:
@@ -127,7 +161,12 @@ def check_tps(row):
 
 
 def check_variance(row):
-    r=[]
+    """
+    Check how is variance of ratings in the session (if the worker just clicked samething)
+    :param row:
+    :return:
+    """
+    r = []
     for q_name in config['question_names']:
         if row[config['gold_question']['url_found_in']] in row[f'answer.{q_name}_url']:
             continue
@@ -138,10 +177,15 @@ def check_variance(row):
 
 
 def check_gold_question(row):
+    """
+    Check if the gold_question is answered correctly
+    :param row:
+    :return:
+    """
     correct_gq = 0
     try:
         gq_url = row[config['gold_question']['url_found_in']]
-        #gq_correct_ans = int(float(row[config['gold_question']['ans_found_in']]))
+        # gq_correct_ans = int(float(row[config['gold_question']['ans_found_in']]))
         gq_correct_ans = config['gold_question']['correct_ans']
         gq_var= config['gold_question']['variance']
         for q_name in config['question_names']:
@@ -152,10 +196,11 @@ def check_gold_question(row):
                     return correct_gq
     except:
         return None
-    return  correct_gq
+    return correct_gq
 
 
 def check_tps_old(row):
+
     found_tps=0
     correct_tps = 0
     tp_urls_contain = list (config['trappings']['correct_answers'])
@@ -176,13 +221,20 @@ def check_tps_old(row):
     return found_tps, correct_tps
 
 
-def check_math(input,output,audio_played):
-    if (audio_played==0):
+def check_math(input, output, audio_played):
+    """
+    check if the math question is answered correctly
+    :param input:
+    :param output:
+    :param audio_played:
+    :return:
+    """
+    if audio_played == 0:
         return False
-    keys= list(config['math'].keys())
+    keys = list(config['math'].keys())
     try:
         for key in keys:
-            if key in input and config['math'][key]==int(output):
+            if key in input and config['math'][key] == int(output):
                 return True
     except:
         return False
@@ -190,6 +242,15 @@ def check_math(input,output,audio_played):
 
 
 def check_a_cmp(file_a, file_b,ans, audio_a_played, audio_b_played):
+    """
+    check if pair comparision answered correctly
+    :param file_a:
+    :param file_b:
+    :param ans:
+    :param audio_a_played:
+    :param audio_b_played:
+    :return:
+    """
     if (audio_a_played==0 or
             audio_b_played == 0):
         return False
@@ -203,30 +264,35 @@ def check_a_cmp(file_a, file_b,ans, audio_a_played, audio_b_played):
         answer_is_correct = True
     elif a == b and ans.strip() == 'o':
         answer_is_correct = True
-   # print (f'({a},{b},{ans})--> {answer_is_correct} ')
     return answer_is_correct
 
 
 def data_cleaning(filename):
+    """
+    Data screening process
+    :param filename:
+    :return:
+    """
     print('Start by Data Cleaning...')
     with open(filename) as csvfile:
 
         reader = csv.DictReader(csvfile)
-        #lowercase the fieldnames
+        # lowercase the fieldnames
         reader.fieldnames = [field.strip().lower() for field in reader.fieldnames]
-        #----------- cmps
-        #Input.CMP1_A	Input.CMP1_B	Input.CMP2_A	Input.CMP2_B	Input.CMP3_A	Input.CMP3_B	Input.CMP4_A	Input.CMP4_B
+        # ----------- cmps
+        # Input.CMP1_A	Input.CMP1_B	Input.CMP2_A	Input.CMP2_B	Input.CMP3_A	Input.CMP3_B	Input.CMP4_A	Input.CMP4_B
         # Answer.cmp1	Answer.cmp2	Answer.cmp3	Answer.cmp4
-        #Answer.audio_n_play_CMP1_A	Answer.audio_n_play_CMP1_B	Answer.audio_n_play_CMP2_A	Answer.audio_n_play_CMP2_B	Answer.audio_n_play_CMP3_A	Answer.audio_n_play_CMP3_B	Answer.audio_n_play_CMP4_A	Answer.audio_n_play_CMP4_B
+        # Answer.audio_n_play_CMP1_A	Answer.audio_n_play_CMP1_B	Answer.audio_n_play_CMP2_A	Answer.audio_n_play_CMP2_B
+        # Answer.audio_n_play_CMP3_A	Answer.audio_n_play_CMP3_B	Answer.audio_n_play_CMP4_A	Answer.audio_n_play_CMP4_B
         # WorkerId
-        #---------------- math
+        # ---------------- math
         # Input.math, Answer.Math,Answer.audio_n_play_math1
-        worker_list=[]
-        accept_and_use_sessions =[]
+        worker_list = []
+        accept_and_use_sessions = []
         for row in reader:
             correct_cmp_ans = 0
             setup_was_hidden = row['answer.cmp1'] is None or len(row['answer.cmp1'].strip()) == 0
-            d = {}
+            d = dict()
 
             d['worker_id'] = row['workerid']
             d['assignment'] = row['assignmentid']
@@ -282,25 +348,41 @@ def data_cleaning(filename):
 
 
 def save_approved_ones(data, path):
+    """
+    save approved results in the given path
+    :param data:
+    :param path:
+    :return:
+    """
     df = pd.DataFrame(data)
     df = df[df.accept == 1]
     small_df = df[['assignment']].copy()
-    small_df.rename(columns={'assignment': 'assignmentId'},
-            inplace=True)
+    small_df.rename(columns={'assignment': 'assignmentId'}, inplace=True)
     small_df.to_csv(path, index=False)
 
 
 def save_rejected_ones(data, path):
+    """
+    Save the rejected ones in the path
+    :param data:
+    :param path:
+    :return:
+    """
     df = pd.DataFrame(data)
     df = df[df.accept == 0]
     small_df = df[['assignment']].copy()
-    small_df.rename(columns={'assignment': 'assignmentId'},
-            inplace=True)
+    small_df.rename(columns={'assignment': 'assignmentId'}, inplace=True)
     small_df= small_df.assign(feedback=config['rejection_feedback'])
     small_df.to_csv(path, index=False)
 
 
-def calc_bonuses(worker_list,path):
+def calc_bonuses(worker_list, path):
+    """
+    Calculate the bonuses given the configurations
+    :param worker_list:
+    :param path:
+    :return:
+    """
     print('Calculate the bonuses...')
     df = pd.DataFrame(worker_list)
     grouped = df.groupby(['worker_id'], as_index=False)['accept'].sum()
@@ -308,18 +390,16 @@ def calc_bonuses(worker_list,path):
 
     # condition  more than 30 hits
     grouped = grouped[grouped.accept >= config['bonus']['when_HITs_more_than']]
-    grouped['bonusAmount']= grouped['accept']*config['bonus']['extra_pay_per_HIT']
+    grouped['bonusAmount'] = grouped['accept']*config['bonus']['extra_pay_per_HIT']
 
     # now find an assignment id
-    df.drop_duplicates('worker_id',keep ='first', inplace = True )
+    df.drop_duplicates('worker_id', keep='first', inplace = True )
     w_ids = list(dict(grouped['worker_id']).values())
     df = df[df.isin(w_ids).worker_id]
     small_df = df[['worker_id', 'assignment']].copy()
 
-    merged= pd.merge( grouped, small_df, how='inner', left_on='worker_id', right_on='worker_id')
-    merged.rename(columns={'worker_id': 'workerId',
-                     'assignment': 'assignmentId'},
-                     inplace=True)
+    merged = pd.merge( grouped, small_df, how='inner', left_on='worker_id', right_on='worker_id')
+    merged.rename(columns={'worker_id': 'workerId', 'assignment': 'assignmentId'}, inplace=True)
 
     merged['reason'] = f'Well done! More than {config["bonus"]["when_HITs_more_than"]} high quality submission'
     merged.to_csv(path, index=False)
@@ -327,6 +407,12 @@ def calc_bonuses(worker_list,path):
 
 
 def write_dict_as_csv(dic_to_write, file_name):
+    """
+    write the dict object in a file
+    :param dic_to_write:
+    :param file_name:
+    :return:
+    """
     with open(file_name, 'w', newline='') as output_file:
         if len(dic_to_write) > 0:
             fieldnames = list(dic_to_write[0].keys())
@@ -341,17 +427,23 @@ def write_dict_as_csv(dic_to_write, file_name):
 file_to_condition_map = {}
 
 
-def filename_to_condition(fname):
-    if fname in file_to_condition_map:
-        return file_to_condition_map[fname]
-    condition_name=""
-    fchar= list(fname)
-    pchars= list(config['condition_pattern'])
-    # "book_04753_chp_0059_reader_10797_6.wav" mask "????????????????????????????????xxxxxx":book_04753_chp_0059_reader_10797
-    for i in range(0,len(pchars)):
-        if pchars[i]=='?':
-            condition_name+=fchar[i]
-    file_to_condition_map[fname]=condition_name
+def filename_to_condition(f_name):
+    """
+    extract the condition name from filename given the mask in the config
+    :param f_name:
+    :return:
+    """
+    if f_name in file_to_condition_map:
+        return file_to_condition_map[f_name]
+    condition_name = ""
+    f_char = list(f_name)
+    p_chars = list(config['condition_pattern'])
+    # "book_04753_chp_0059_reader_10797_6.wav"
+    #  --> "????????????????????????????????xxxxxx":book_04753_chp_0059_reader_10797
+    for i in range(0, len(p_chars)):
+        if p_chars[i] == '?':
+            condition_name += f_char[i]
+    file_to_condition_map[f_name] = condition_name
     return condition_name
 
 
@@ -360,10 +452,11 @@ def transform(sessions, path_per_file, path_per_condition):
     Given the valid sessions from answer.csv, group votes per files, and per conditions.
     Assumption: file name starts with Cxx where xx is condition number.
     :param sessions:
+    :param path_per_file:
+    :param path_per_condition:
     :return:
     """
-
-    print ("Transforming data (the ones with 'accepted_and_use' ==1 --> group per clip")
+    print("Transforming data (the ones with 'accepted_and_use' ==1 --> group per clip")
     data_per_file = {}
     data_per_condition = {}
     for session in sessions:
@@ -412,13 +505,14 @@ def transform(sessions, path_per_file, path_per_condition):
             tmp['95%CI'] = None
         group_per_file.append(tmp)
 
-
     # convert the format: one row per condition
     group_per_condition = []
     for key in data_per_condition.keys():
-        tmp = {}
+        tmp = dict()
         tmp['condition_name'] = key
         votes = data_per_condition[key]
+        # apply z-score outlier detection
+        votes = outliers_z_score(votes)
 
         tmp['n'] = len(votes)
         tmp['mean'] = statistics.mean(votes)
@@ -426,17 +520,20 @@ def transform(sessions, path_per_file, path_per_condition):
         tmp['95%CI'] = (1.96 * tmp['std']) / math.sqrt(tmp['n'])
         group_per_condition.append(tmp)
 
-    #return group_per_file, group_per_condition
-
+    # return group_per_file, group_per_condition
     write_dict_as_csv(group_per_file, path_per_file)
     write_dict_as_csv(group_per_condition, path_per_condition)
     print(f'   Votes per files are saved in: {path_per_file}')
     print(f'   Votes per files are saved in: {path_per_condition}')
-
     return group_per_file, group_per_condition
 
 
 def initiate_file_row(d):
+    """
+    add difault values in the dict
+    :param d:
+    :return:
+    """
     d['file_name'] = None
     d['n'] = None
     d['mean'] = None
@@ -445,35 +542,6 @@ def initiate_file_row(d):
     for i in range(1, config['expected_votes_per_file']+1):
         d[f'vote_{i}'] = None
     return d
-
-
-def data_import(filename):
-    with open(filename) as csvfile:
-        reader = csv.DictReader(csvfile)
-        # lowercase the fieldnames
-        reader.fieldnames = [field.strip().lower() for field in reader.fieldnames]
-
-        data_per_condition = {}
-        for row in reader:
-            condition = row['filename'][0:3]
-            if condition not in data_per_condition:
-                data_per_condition[condition] = []
-            data_per_condition[condition].append(int(row['mos']))
-
-        print (data_per_condition)
-
-        group_per_condition = []
-        for key in data_per_condition.keys():
-            tmp = {}
-            tmp['condition_name'] = key
-            votes = data_per_condition[key]
-
-            tmp['n'] = len(votes)
-            tmp['mean'] = statistics.mean(votes)
-            tmp['std'] = statistics.stdev(votes)
-            tmp['95%CI'] = (1.96 * tmp['std']) / math.sqrt(tmp['n'])
-            group_per_condition.append(tmp)
-        return group_per_condition
 
 
 if __name__ == '__main__':
@@ -489,20 +557,8 @@ if __name__ == '__main__':
 
     accepted_sessions = data_cleaning(args.answers)
 
-    #votes_per_file, votes_per_condition = transform(accepted_sessions)
-
-    if (len(accepted_sessions)> 1):
+    # votes_per_file, votes_per_condition = transform(accepted_sessions)
+    if len(accepted_sessions) > 1:
         votes_per_file_path = os.path.splitext(args.answers)[0] + '_votes_per_clip.csv'
         votes_per_cond_path = os.path.splitext(args.answers)[0] + '_votes_per_cond.csv'
         votes_per_file, vote_per_condition = transform(accepted_sessions, votes_per_file_path, votes_per_cond_path)
-
-
-        """
-        write_dict_as_csv(votes_per_condition, 'votes_per_condition.csv')
-    
-        votes_per_condition_psamd1 = data_import('psamd1.csv')
-        write_dict_as_csv(votes_per_condition_psamd1, 'votes_per_condition_psamd1.csv')
-        """
-    a = [57, 59, 60, 100, 59, 58, 57, 58, 300, 61, 62, 60, 62, 58, 57];
-    o= outliers_modified_z_score(a)
-    print(o)
